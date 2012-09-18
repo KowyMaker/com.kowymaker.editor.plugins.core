132017cc41a071a9487f16dd237d66dd11e35f09
KokaKiwi <admin@kokabsolu.com>
2012-09-18 02:53:34 +0200

    First commit. Plugin base.

---
+++ src/main/java/com/kowymaker/editor/plugins/core/Application.java
@@ -0,0 +1,23 @@
+package com.kowymaker.editor.plugins.core;
+
+import org.eclipse.equinox.app.IApplication;
+import org.eclipse.equinox.app.IApplicationContext;
+
+public class Application implements IApplication
+{
+    
+    @Override
+    public Object start(IApplicationContext context) throws Exception
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    @Override
+    public void stop()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
